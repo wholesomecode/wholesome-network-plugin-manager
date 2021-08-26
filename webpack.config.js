bd93c049249b2db11c09d80d1a39a7b964f867dd
Matt Watson <matt.watson@iconicwp.com>
2021-08-26 21:17:22 +0100

    Implemented build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 const path = require( 'path' );
+const ZipFilesPlugin = require('webpack-zip-files-plugin');
 
 module.exports = {
     ...defaultConfig,
     entry: {
         admin: path.resolve( process.cwd(), 'src', 'admin.js' ),
-    }
+    },
+    plugins: [
+        ...defaultConfig.plugins,
+        new ZipFilesPlugin({
+            entries: [
+                { src: path.join(__dirname, './build'), dist: 'build' },
+                { src: path.join(__dirname, './inc'), dist: 'inc' },
+                { src: path.join(__dirname, './languages'), dist: 'languages' },
+                { src: path.join(__dirname, './plugin.php'), dist: 'plugin.php' },
+            ],
+            output: path.join(__dirname, './dist/wholesome-network-plugin-manager'),
+            format: 'zip',
+        }),
+    ],
 };
